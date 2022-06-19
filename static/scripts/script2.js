cfcab75ef83c413aca1581635933bc68b7c9ba45
VictordotCC <victor.celis.c88@gmail.com>
2022-06-18 21:34:51 -0400

    varios

--- static/scripts/script2.js
+++ static/scripts/script2.js
@@ -1,7 +1,10 @@
 const url = localStorage.getItem('url');
 $(document).ready(function() {
 
-    carrito = JSON.parse(localStorage.getItem('carrito'));
+
+    var carrito = JSON.parse(localStorage.getItem('carrito'));
+    console.log(carrito);
+
     $.ajax({
         url: `${url}/carrito`,
         type: 'POST',
@@ -10,9 +13,20 @@ $(document).ready(function() {
         },
         success: function(data) {
             console.log(data);
+            data.forEach(function(producto) {
+                product_html = 
+                `   <tr><td scope="col">${producto.nombre}</td>
+                    <td scope="col">$${producto.valor_venta.toLocaleString('es-CL')}</td>
+                    <td scope="col">${producto.cantidad}</td>
+                    <td scope="col">${producto.valor * producto.cantidad}</td>
+                    <td scope="col" style="color: rgb(250, 1, 1)"><i class="fa-solid fa-delete-left ps-4 clickable" id="eliminar"></i></td>
+                    </tr>`;
+                $('#productos').append(product_html);
+                }
+            );
             //for each y cargar los productos en la pagina
 
-        }
+        }     
     });
 
     /*$('#eliminar').click(function() {
