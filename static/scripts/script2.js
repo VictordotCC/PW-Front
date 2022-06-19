f7fa31e8df2f61d3d394eb02e61db4677d5717a3
VictordotCC <victor.celis.c88@gmail.com>
2022-06-18 22:02:38 -0400

    carrito terminado

--- static/scripts/script2.js
+++ static/scripts/script2.js
@@ -12,26 +12,39 @@ $(document).ready(function() {
             carrito: carrito
         },
         success: function(data) {
-            console.log(data);
+            var subtotal = 0;
+            var iva;
+            var total;
+
             data.forEach(function(producto) {
                 product_html = 
                 `   <tr><td scope="col">${producto.nombre}</td>
                     <td scope="col">$${producto.valor_venta.toLocaleString('es-CL')}</td>
                     <td scope="col">${producto.cantidad}</td>
-                    <td scope="col">${producto.valor * producto.cantidad}</td>
-                    <td scope="col" style="color: rgb(250, 1, 1)"><i class="fa-solid fa-delete-left ps-4 clickable" id="eliminar"></i></td>
+                    <td scope="col">$${(producto.valor_venta * producto.cantidad).toLocaleString('es-CL')}</td>
+                    <td scope="col" style="color: rgb(250, 1, 1)"><i class="fa-solid fa-delete-left ps-4 clickable eliminar"></i></td>
                     </tr>`;
                 $('#productos').append(product_html);
+                subtotal += producto.valor_venta * producto.cantidad;
                 }
             );
-            //for each y cargar los productos en la pagina
+            $('#subtotal').text("$"+ subtotal.toLocaleString('es-CL'));
+            $('#iva').text("$"+ (Math.round(subtotal * 0.19)).toLocaleString('es-CL'));
+            $('#total').text("$"+ (Math.round(subtotal * 1.19)).toLocaleString('es-CL'));
+
+            $('.eliminar').click(function() {
+                $(this).parent().parent().remove();
+                subtotal -= $(this).parent().parent().children().eq(3).text().replace(/[^0-9]/g, '');
+                $('#subtotal').text("$"+ subtotal.toLocaleString('es-CL'));
+                iva = Math.round(subtotal * 0.19);
+                $('#iva').text("$"+ iva.toLocaleString('es-CL'));
+                total = subtotal + iva;
+                $('#total').text("$"+ total.toLocaleString('es-CL'));
+            });
+            
 
         }     
     });
 
-    /*$('#eliminar').click(function() {
-        $('#productos').children().each(function(index, el) {
-            $(el).remove();
-        });
-    });*/
+
 });
